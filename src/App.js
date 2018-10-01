afa91d0bb141b8445ba4cb6602b40b45ab9898a6
Kena Benioni <kenabenioni@Kenas-MacBook-Pro.local>
2018-10-01 16:40:46 -0600

    progress

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     return (
       <div className="App">
       <Nav/>
+      <div>
         {routes}
+      </div>
       <Footer/>
       </div>
     );
--- src/components/ViewAll/ViewAll.js
+++ src/components/ViewAll/ViewAll.js
@@ -1,41 +1,55 @@
 import React, { Component } from "react";
-import './ViewAll.css';
+import "./ViewAll.css";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 class ViewAll extends Component {
   constructor(props) {
-    super(props) 
-      this.state = {
-        imgs: []
-      }
+    super(props);
+    this.state = {
+      imgs: []
+    };
   }
 
-  threeImgs(){
-    axios.get('/api/threeimgs', {product_id: this.props.id, color_id: this.props.color_id})
-    .then(response=>{
-      this.setState({
-        imgs: response.data
+  threeImgs() {
+    axios
+      .get("/api/threeimgs", {
+        product_id: this.props.id,
+        color_id: this.props.color_id
       })
-    })
+      .then(response => {
+        this.setState({
+          imgs: response.data
+        });
+      });
   }
 
+  render() {
+    const {
+      name,
+      id,
+      brand,
+      category,
+      color_id,
+      details,
+      img_id,
+      img,
+      price,
+      subcategory
+    } = this.props;
 
-
-  render(){
-    
-    const { name, id, brand, category, color_id, details, img_id, img, price, subcategory } = this.props;
-    
     return (
       <Link to={`/products/${id}/${img_id}`}>
-      <div className="ViewAll">
-      <img src={img} className="product_img" alt=""/>
-      <div>
-          <p>{brand}</p>
-          <p>{name}</p>
-          <p>${price}</p>
-      </div>
-      </div>
+        <div className="ViewAll">
+        <div className="product">
+          <img src={img} className="product_img" alt="" />
+          <div>
+            <p>{brand}</p>
+            <p>{name}</p>
+            <p>${price}</p>
+          </div>
+        </div>
+        </div>
       </Link>
     );
   }
